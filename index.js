ba42b97d02e3895146fea577371d42dc8cd74ae9
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-20 15:44:39 -0700

    added nav bar, links homepage

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 //require stuff
 var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var express = require('express');
 
 //declare global variables
 var app = express();
 
 //set use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+
+//include controllers/routers
+app.use('/articles', require('./controllers/articles'));
+app.use('/authors', require('./controllers/authors'));
 
 //define routes
-app.get(function(req, res){
-	res.render('/');
+app.get('/', function(req, res){
+	res.render('home');
 });
 
 //listen to port 3000

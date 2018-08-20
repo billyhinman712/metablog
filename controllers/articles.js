5ce431a045e8c1351f3b66aded1b91d0d24cd56d
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-20 15:58:07 -0700

    created routes for authors and articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.send('hello');
+	res.render('articles/index');
 });
+
+router.get('/new', function(req, res){
+	res.send('new article form goes here');
+});
+
+router.get('/:id', function(req, res){
+	res.send('article show page');
+});
+
+router.post('/', function(req, res){
+	res.send('/article POST route reahced');
+});
+
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.send('hello');
+	res.render('authors/index');
+});
+
+router.get('/new', function(req, res){
+	res.send('new author form goes here');
+});
+
+router.get('/:id', function(req, res){
+	res.send('author show page');
+});
+
+router.post('/', function(req, res){
+	res.send('/authors POST route reahced');
 });
 
 module.exports = router;

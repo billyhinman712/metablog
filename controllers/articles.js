9c07faf2bca4e49ea05d0c98a4edfdd8ea4b7197
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-21 15:41:52 -0700

    added tags to articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -23,7 +23,7 @@ router.get('/new', function(req, res){
 router.get('/:id', function(req, res){
 	db.article.findOne({
 		where: {id: req.params.id},
-		include: [db.author, db.comment]
+		include: [db.author, db.comment, db.tag]
 	}).then(function(foundArticle){
 		db.author.findAll().then(function(allAuthors){
 			res.render('articles/show', {article: foundArticle, authors: allAuthors});
@@ -41,7 +41,24 @@ router.post('/', function(req, res){
 	if(req.body.authorId !== 0){
 		console.log(req.body);
 		db.article.create(req.body).then(function(createdArticle){
+			//parse the tags if any
+			var tags = [];
+			if(req.body.tags){
+				tags = req.body.tags.split(',');
+			}
+
+			if(tags.length > 0){
+				tags.forEach(function(t){
+					db.tag.findOrCreate({
+						where: {name: t.trim()}
+					}).spread(function(newTag, wasCreated){
+						createdArticle.addTag(newTag);
+					});
+				});
+				res.redirect('/articles/' + createdArticle.id);
+			}else{
 			res.redirect('/articles/' + createdArticle.id);
+		}
 		}).catch(function(err){
 			console.log(err);
 			res.render('error');
